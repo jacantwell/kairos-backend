d6e4d967daef17041087aef7d65a3ac39cd0f9bd
Jasper Cantwell <jasper66018@gmail.com>
2025-09-07 09:11:37 +0100

    Including new routes

--- kairos/api/main.py
+++ kairos/api/main.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
-from kairos.api.routes import auth_router, root_router, users_router
+from kairos.api.routes import auth_router, root_router, users_router, journeys_router
 from kairos.core.config import settings
 
 api_router = APIRouter()
 api_router.include_router(auth_router)
 api_router.include_router(users_router)
 api_router.include_router(root_router)
+api_router.include_router(journeys_router)
 
 
 if settings.ENVIRONMENT == "local":
--- kairos/api/routes/__init__.py
+++ kairos/api/routes/__init__.py
@@ -1,3 +1,4 @@
 from kairos.api.routes.auth import router as auth_router
 from kairos.api.routes.root import router as root_router
 from kairos.api.routes.users import router as users_router
+from kairos.api.routes.journeys import router as journeys_router
