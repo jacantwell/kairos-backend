11d0697f0250eceb257d0eb5ca99dc627a72c332
Jasper Cantwell <jasper66018@gmail.com>
2025-11-09 18:47:05 +0000

    Reverting auth status code change back to 400

--- kairos/api/routes/auth.py
+++ kairos/api/routes/auth.py
@@ -25,7 +25,7 @@ async def login(
         data: OAuth2 password request form containing username (email) and password.
 
     Raises:
-        HTTPException: 401 if username or password is incorrect.
+        HTTPException: 400 if username or password is incorrect.
 
     Returns:
         Tokens: Object containing access_token and refresh_token.
@@ -33,12 +33,12 @@ async def login(
     found_users = await db.users.query({"email": data.username})
 
     if len(found_users) == 0:
-        raise HTTPException(status_code=401, detail="Incorrect username or password")
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
     else:
         user = found_users[0]
 
     if not verify_password(data.password, user.password):
-        raise HTTPException(status_code=401, detail="Incorrect username or password")
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
 
     access_token = create_token(
         subject=user.id,
@@ -66,7 +66,7 @@ async def refresh(refresh_token: str) -> Tokens:
         refresh_token: Valid JWT refresh token.
 
     Raises:
-        HTTPException: 401 if refresh token has expired.
+        HTTPException: 400 if refresh token has expired.
         HTTPException: 403 if refresh token is invalid.
 
     Returns:
@@ -79,7 +79,7 @@ async def refresh(refresh_token: str) -> Tokens:
     try:
         sub = decode_token(refresh_token, scope="refresh")
     except ExpiredSignatureError:
-        raise HTTPException(status_code=401, detail="Refresh token has expired")
+        raise HTTPException(status_code=400, detail="Refresh token has expired")
     except Exception as e:
         raise HTTPException(status_code=403, detail="Invalid refresh token")
     new_access = create_token(
